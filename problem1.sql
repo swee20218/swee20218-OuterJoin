981b637ea92bd9b2feddf0491dc9427132d32502
Sweethakanayiram <swee20218.it@rmkec.ac.in>
2024-08-25 12:58:30 +0530

    outer join done

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+select * from class
+left join student
+on class.class_title = student.class_title;
---
+++ problem2.sql
@@ -0,0 +1,3 @@
+select * from class
+right join textbook
+on class.class_title = textbook.class_title;
